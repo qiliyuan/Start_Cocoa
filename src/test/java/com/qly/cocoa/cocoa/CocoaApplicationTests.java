a48107af2f05f8bc4a42e5cd43a8cae2fbf43aba
齐立圆 <460570129@qq.com>
2018-05-29 15:34:08 +0800

    第一次提交

---
+++ src/test/java/com/qly/cocoa/cocoa/CocoaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qly.cocoa.cocoa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CocoaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
