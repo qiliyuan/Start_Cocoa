e0ac34bb80cb6e7ad4fbe723aa954df84f228543
齐立圆 <460570129@qq.com>
2018-06-01 10:04:41 +0800

    aaa

---
+++ src/main/java/com/qly/cocoa/App.java
@@ -0,0 +1,16 @@
+package com.qly.cocoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@SpringBootApplication(
+		exclude= {DataSourceAutoConfiguration.class})
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Cocoa开始启动Spring boot");
+		SpringApplication.run(App.class, args);
+		System.out.println("Cocoa启动完毕");
+	}
+}
