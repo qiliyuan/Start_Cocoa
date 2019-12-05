941c3e7e60b07cd4d03fed7fa5896acb170aa3b2
qiliyuan <qiliyuan@hzzl.net>
2019-12-05 16:38:38 +0800

    idea修个测试push23

--- src/main/java/com/qly/cocoa/controller/HomeController.java
+++ src/main/java/com/qly/cocoa/controller/HomeController.java
@@ -13,6 +13,7 @@ public class HomeController {
 		System.out.println("修改测试controller");
 
 		System.out.println("修改测试controller");
+		System.out.println("修改测试controller");
 		return "/blog/index";
 	}
 }
